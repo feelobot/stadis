a7eb915605cc1ec6b1d7fd2af345e67329e71892
Felix Rodriguez <felix.a.rod@gmail.com>
2015-10-08 15:02:46 -0700

    working

--- stadis.go
+++ stadis.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/cactus/go-statsd-client/statsd"
 	"github.com/codegangsta/cli"
-	"github.com/quipo/statsd"
+	"github.com/fatih/color"
 	"gopkg.in/redis.v3"
 	"os"
 	"regexp"
 	"strconv"
 	"time"
 )
-
-var prefix = os.Getenv("HOSTNAME")
-var statsdclient = statsd.NewStatsdClient("localhost:8125", prefix)
 
 func main() {
 	// init
@@ -40,9 +38,13 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) {
-		info := getStats(c.String("redis-host"))
-		gauges := parseGauges(info)
-		sendGauges(gauges)
+		for {
+			info := getStats(c.String("redis-host"))
+			gauges := parseGauges(info)
+			sendGauges(gauges, c.String("statsd-host"))
+			color.Yellow("-------------------")
+			time.Sleep(5000 * time.Millisecond)
+		}
 	}
 	app.Run(os.Args)
 }
@@ -55,23 +57,24 @@ func getStats(addrs string) string {
 	})
 	info, err := client.Info().Result()
 	if err != nil {
-		fmt.Println("Error connecting to redis") //possibly send to statsd also
+		color.Red("ERROR: can't connect to redis") //possibly send to statsd also
 	}
-	//fmt.Println(info)
+	// DEBUG: fmt.Println(info)
 	return info
 }
 
 func sendStats() {
 }
-func sendGauges(gauges map[string]int64) {
-	statsdclient.CreateSocket()
-	interval := time.Second * 5 // aggregate stats and flush every 2 seconds
-	stats := statsd.NewStatsdBuffer(interval, statsdclient)
-	defer stats.Close()
-
-	for k, v := range gauges {
-		fmt.Println("k:", k, "v:", v)
-		stats.Gauge("k", v)
+func sendGauges(gauges map[string]int64, statsdHost string) {
+	color.Yellow("-------------------")
+	color.White("GAUGES:")
+	client, err := statsd.NewClient(statsdHost, "test-client")
+	if err != nil {
+		color.Red("ERROR: can't connect to statsd")
+	}
+	defer client.Close()
+	for gauge, value := range gauges {
+		client.Gauge(gauge, value, 1.0)
 	}
 
 }
@@ -79,28 +82,32 @@ func statify(info string) {
 }
 
 func parseGauges(info string) map[string]int64 {
-	var gauges_with_values map[string]int64
-	gauges := []string{
-		"blocked_clients",
-		"connected_clients",
-		"instantaneous_ops_per_sec",
-		"latest_fork_usec",
-		"mem_fragmentation_ratio",
-		"migrate_cached_sockets",
-		"pubsub_channels",
-		"pubsub_patterns",
-		"uptime_in_seconds",
-		"used_memory",
-		"used_memory_lua",
-		"used_memory_peak",
-		"used_memory_rss",
+	gauges_with_values := map[string]int64{
+		"blocked_clients":           0,
+		"connected_clients":         0,
+		"instantaneous_ops_per_sec": 0,
+		"latest_fork_usec":          0,
+		"mem_fragmentation_ratio":   0,
+		"migrate_cached_sockets":    0,
+		"pubsub_channels":           0,
+		"pubsub_patterns":           0,
+		"uptime_in_seconds":         0,
+		"used_memory":               0,
+		"used_memory_lua":           0,
+		"used_memory_peak":          0,
+		"used_memory_rss":           0,
 	}
-	for _, gauge := range gauges {
+	for gauge, _ := range gauges_with_values {
 		r, _ := regexp.Compile(fmt.Sprint(gauge, ":([0-9]*)"))
-		value := r.FindStringSubmatch(info)[1]
-		fmt.Println(fmt.Sprint(gauge, ": ", value))
-		v, _ := strconv.ParseInt(value, 10, 64)
-		gauges_with_values[gauge] = v
+		matches := r.FindStringSubmatch(info)
+		if matches == nil {
+			color.Red(fmt.Sprint("ERROR: ", gauge, "is not displayed in redis info"))
+		} else {
+			value := matches[len(matches)-1]
+			color.Cyan(fmt.Sprint(gauge, ": ", value))
+			v, _ := strconv.ParseInt(value, 10, 64)
+			gauges_with_values[gauge] = v
+		}
 	}
 	return gauges_with_values
 }
